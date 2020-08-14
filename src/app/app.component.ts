7f0bb375913fddc273412637e76fd230b5505f70
edwin <easd2425@gmail.com>
2020-08-14 15:09:31 -0400

    exercicio 02 de angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ejercicio';
+  user: Array<any>;
+
+  constructor(private userService: UserService) {
+    this.user = new Array<any>();
+  }
+
+  getAllUsers() {
+    this.userService.getUser().subscribe((user) => {
+      this.user = user;
+    });
+  }
 }
